ee5aac44c2ba851f0d72772867ac37a76b4e326e
MBUVI DAVID <147725809+cheekydavy@users.noreply.github.com>
2024-09-07 11:04:24 +0300

    Update id.js

--- id.js
+++ id.js
@@ -1,8 +1,8 @@
 function makeid(num = 4) {
-  let result = "";
+  let result = "SUHAIL_06_26_09_06_";
   let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  var characters9 = characters.length;
-  for (var i = 0; i < num; i++) {
+  let characters9 = characters.length;
+  for (let i = 0; i < num; i++) {
     result += characters.charAt(Math.floor(Math.random() * characters9));
   }
   return result;
